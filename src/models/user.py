c9d0f52ec4814a7243c417c379780483ee7ab782
Filipe <filipeccoutinho2017@gmail.com>
2025-06-03 20:20:48 -0300

    realinhamento do endpoints

--- src/models/user.py
+++ src/models/user.py
@@ -4,7 +4,7 @@ from .base import db
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(sa.String,unique=True)
+    username: Mapped[str] = mapped_column(sa.String, unique=True)
     active: Mapped[bool] = mapped_column(sa.Boolean, default=True)
     password: Mapped[str] = mapped_column(sa.String, nullable=False)
     role_id: Mapped[int] = mapped_column(sa.ForeignKey("role.id"))
